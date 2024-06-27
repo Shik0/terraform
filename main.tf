08f0c0e10880b2954c73e86da1bc68e883f288bf
Shahin Mikayilov <shahin.mikayilov@dynatrace.com>
2024-06-27 23:06:29 +0200

    Add modules

--- main.tf
+++ main.tf
@@ -261,3 +261,15 @@ resource "aws_security_group" "vpc-ping" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+module "server" {
+  source      = "./modules/web_server"
+  ami         = data.aws_ami.ubuntu.id
+  key_name    = aws_key_pair.generated.key_name
+  user        = "ubuntu"
+  private_key = tls_private_key.generated.private_key_pem
+  subnet_id   = aws_subnet.public_subnets["public_subnet_1"].id
+  security_groups = [aws_security_group.vpc-ping.id,
+    aws_security_group.ingress-ssh.id,
+  aws_security_group.vpc-web.id]
+}
