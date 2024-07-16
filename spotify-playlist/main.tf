a86309b7781a0c51e7f919bc84dc7d06bc9fdc5a
Shahin Mikayilov <shahin.mikayilov@dynatrace.com>
2024-07-16 15:15:27 +0200

    Create a spotify playlist with TF

---
+++ spotify-playlist/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    spotify = {
+      version = "~> 0.1.5"
+      source  = "conradludgate/spotify"
+    }
+  }
+}
+
+provider "spotify" {
+  api_key = var.spotify_api_key
+}
+
+data "spotify_search_track" "by_artist" {
+  artists = ["Rammstein"]
+  #  album = "album"
+  #  name  = "song-name"
+}
+
+resource "spotify_playlist" "playlist" {
+  name        = "Terraform Summer-Rock Playlist"
+  description = "This playlist was created by Terraform"
+  public      = true
+
+  tracks = [
+    data.spotify_search_track.by_artist.tracks[0].id,
+    data.spotify_search_track.by_artist.tracks[1].id,
+    data.spotify_search_track.by_artist.tracks[2].id,
+  ]
+}
