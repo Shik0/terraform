0595a94e148dd52af9657c77ff35f3cec8cb96d2
shik0 <mikailovsh@gmail.com>
2024-07-23 23:25:17 +0200

    apply webapp on AWS

---
+++ deploy_web_app_on_AWS/terraform.tf
@@ -0,0 +1,19 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+cloud {
+    organization = "mikayilov"
+    workspaces {
+      name = "mikayilov"
+    }
+}
+
+required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.55.0"
+    }
+  }
+  required_version = "~> 1.2"
+}
